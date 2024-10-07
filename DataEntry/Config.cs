cc7ba47cd356fb02ffe5dcfc1f9210a55a8aa854
David York <david@davideyork.com>
2024-10-07 11:09:17 -0700

    Removing Avalonia project because it was not used

--- DataEntry/Config.cs
+++
@@ -1,7 +0,0 @@
-public static class Config {
-    public static string TestJsonFolder => $"{resources}/test_json/";
-
-    // Private
-
-    private const string resources = "../Assets/Resources";
-}
